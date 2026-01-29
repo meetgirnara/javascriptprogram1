63585cc199ebf5d1ce7837ef52fd90b517d58266
manav girnara <manavgirnara@gmail.com>
2026-01-29 13:29:18 +0530

    object file are added

--- obj.js
+++ obj.js
@@ -39,3 +39,74 @@ console.log(student);
 console.log(Object.keys(student));
 console.log(Object.values(student));
 console.log(Object.entries(student));
+
+//
+
+const user ={
+    name : "Meet",
+    role : "Admin",
+    designation : "Developer",
+    age : 20,
+    address : "keshod"
+}
+const{ name , role , designation} = user;
+console.log(name );
+console.log(role );
+console.log(designation );
+
+const{name :profilename, age : profileage, address : profileaddress} = user;
+console.log(profilename);
+console.log(profileage);
+console.log(profileaddress);
+
+const array = [1,2,3,4,5];
+const moreElements = [...array ,6,7,8,9,10];
+console.log(moreElements);
+
+const originalArray = [1,2,3,4,5];
+const copiedArray = [...originalArray];
+console.log(copiedArray);
+
+const arr1 = [1,2,3];
+const arr2 = [4,5,6];
+const mergeArray = [...arr1, ...arr2];
+console.log(mergeArray);
+
+// spread opt. in object
+
+const user1 = {
+    name : "Meet",
+    role : "Admin"
+}
+
+console.log(user1);
+const newObj = {
+    ...user1, designation : "Developer",
+    age : 20
+};
+console.log(newObj);
+
+//copy object
+const user2 = {
+    name : "Meet",
+    role : "Admin"
+}
+
+const copiedObj = {...user2};
+console.log("copiedObj ",copiedObj);
+
+//merge object
+const obj1 = {
+    name : "Meet",
+    role : "Admin",
+    age : 20,
+}
+
+const obj2 = {
+    designation : "Developer",
+    age : 25
+}
+
+const obj3 = {...obj1, ...obj2};
+console.log("mergedObj", obj3);
+
